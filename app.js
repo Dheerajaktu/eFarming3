60d26b228f4681c3ab1de28458988b3652ce560b
Dheeraj-jft <dheeraj.sharma@jellyfishtechnologies.com>
2021-06-18 18:19:16 +0530

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
-
+const port = process.env.PORT || 4000;
 var app = express();
 
 // view engine setup
@@ -22,20 +22,9 @@ app.use(express.static(path.join(__dirname, 'src/public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(port, () => {
+  console.log('server started at port 4000');
+})
 
 module.exports = app;
